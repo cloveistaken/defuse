058488da3dc1d1a5d5041b18fdf1c0670b6c64fd
cloveistaken <cloveistaken@protonmail.com>
2020-06-17 23:14:50 +0200

    Done - Phase 6

--- src/phase_6.c
+++ src/phase_6.c
@@ -24,15 +24,58 @@ solve_phase_6 (Bomb* bomb)
   answer[ANSWER_MAX_LEN] = '\0';
 
   /* Generating possible answers here */
-  char* trial = "Wow! Brazil is big.";
-  strncpy(answer, trial, ANSWER_MAX_LEN);
-  
-  if (try_answer(file, answer) == 0)
-    {
-      FOUND(answer);
-      bomb->answer[PHASE_6] = answer;
-    }
+  int i1, i2, i3, i4, i5, i6;
+  for (i1 = 1; i1 <= 6; i1++)
+    for (i2 = 1; i2 <= 6; i2++)
+      {
+        if (i2 == i1)
+          continue;
 
+        for (i3 = 1; i3 <= 6; i3++)
+          {
+            if (i3 == i1 || i3 == i2)
+              continue;
+
+            for (i4 = 1; i4 <= 6; i4++)
+              {
+                if (i4 == i1 || i4 == i2 || i4 == i3)
+                  continue;
+
+                for (i5 = 1; i5 <= 6; i5++)
+                  {
+                    if (i5 == i1 || i5 == i2 || i5 == i3 || i5 == i4)
+                      continue;
+
+                    for (i6 = 1; i6 <= 6; i6++)
+                      {
+                        if (i6  == i1 || i6 == i2 || i6 == i3 || i6 == i4 || i6 == i5)
+                          continue;
+
+                        snprintf(answer, ANSWER_MAX_LEN + 1, "%d %d %d %d %d %d",
+                                 i1, i2, i3, i4, i5, i6);
+
+                        if (try_answer(file, answer) == 0)
+                          {
+                            FOUND(answer);
+                            bomb->answer[PHASE_6] = answer;
+                            goto done_brute_6;
+                          }
+                      }
+                  }
+              }
+          }
+      }
+
+  /*
+  int i[6];
+  for (int i[0] = 1; i[0] <= 6; i[0]++)
+    for (int i[1] = 1; i[1] <= 6; i[1]++)
+      for (int i[2] = 1; i[2] <= 6; i[2]++)
+        for (int i[3] = 1; i[3] <= 6; i[3]++)
+          for (int i[4] = 1; i[4] <= 6; i[4]++)
+            for (int i[5] = 1; i[5] <= 6; i[5]++)
+            */
+done_brute_6:
   if (bomb->answer[PHASE_6] == NULL)
     ERROR("Can't find answer for phase %d.", PHASE_6);
 
